81ef73ae9f822cea5c6c48db807e576aa7854951
Edward Salinas <edwsalr.es@gmail.com>
2025-04-30 03:49:25 -0400

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const useCounter = (initialValue = 10) => {
+    const [counter, setCounter] = useState(initialValue);
+    
+    const incremet = ( value = 1 ) => {
+        setCounter((beforeCounter) => beforeCounter + value);
+    };
+
+    const decrement = (value = 1) => {
+        // if (counter === 0) return;
+        setCounter((beforeCounter) => beforeCounter - value);
+    };
+
+    const reset = () => {
+        setCounter(initialValue);
+    }
+    return {
+        counter,
+        incremet,
+        decrement,
+        reset,
+    };
+};
---
+++ useTodo.js
@@ -0,0 +1,58 @@
+import { useEffect, useReducer } from "react";
+import {todoReducer} from '../08-useReducer/todoReducer'
+
+
+const initialState = [];
+
+const init = () => {
+    return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodo = () => {
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        console.log(todos);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    const HandleNewTodo = (todo) => {
+        const action = {
+            type: "[TODO] Add Todo",
+            payload: todo,
+        };
+
+        dispatch(action);
+    };
+
+    const HandleDeleteTodo = (id) => {
+        dispatch({
+            type: "[TODO] Remove Todo",
+            payload: id,
+        });
+    };
+
+    const HandleToogleTodo = (id) => {
+        dispatch({
+            type: "[TODO] Toogle Todo",
+            payload: id,
+        });
+    };
+
+    const todosCount = () => {
+        return todos.length;
+    }
+
+    const pendingTodosCount = () => {
+        return todos.filter(todo => !todo.done).length
+    }
+
+    return {
+        todos,
+        HandleDeleteTodo,
+        HandleNewTodo,
+        HandleToogleTodo,
+        todosCount, 
+        pendingTodosCount,
+    };
+};
